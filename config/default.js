2fade8e433a66e9e64685f2be25cfea637780380
oakfire <oakwatcher@gmail.com>
2014-08-11 07:34:20 +0000

    project init

---
+++ config/default.js
@@ -0,0 +1,10 @@
+module.exports = {
+    /** 监听端口 */
+    "listen_port": 3000,
+    /** 日志路径(绝对路径) */
+    //"log_dir":"",
+    /** 日记记录是否不输出到控制台,生产环境请设置为true */
+    "console_quiet": false,
+    /** 日志级别设置, 值可为"silly" "debug" "verbose" "info" "warn" "error" 之一,默认为"debug" */ 
+    "log_level": "debug"
+};
