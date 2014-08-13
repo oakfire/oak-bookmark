2654956494dee6d792dd7c05326a4777d40fa199
oakfire <oakwatcher@gmail.com>
2014-08-13 10:02:08 +0000

    jade init

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var config = require('../lib/config');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: config.site_name });
 });
 
 module.exports = router;
