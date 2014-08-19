eafb2ebc068ee9b424ab7ee9dc57befefcd98f03
oakfire <oakwatcher@gmail.com>
2014-08-19 08:37:24 +0000

    passport

--- routes/index.js
+++ routes/index.js
@@ -4,28 +4,32 @@ var config = require('../lib/config');
 
 /* GET home page. */
 
-function _doNormal(res) {
-  var data = { title: config.site_name,
-      login: false,
-      page_name: 'home'
-  };
-  res.render('index', data);
+function _doNormal(req, res) {
+    var data = { title: config.site_name,
+        login: req.isAuthenticated()
+    };
+    return data;
 }
 
 router.get('/', function(req, res) {
-    _doNormal(res);
+    res.redirect('/index');
 });
 
 router.get('/index', function(req, res) {
-    _doNormal(res);
+    var data = _doNormal(req, res);
+    data.page_name = 'home';
+    res.render('index', data);
 });
 
 router.get('/about', function(req, res) {  
-  var data = { title: config.site_name,
-      login: false,
-      page_name: 'about'
-  };
-  res.render('about', data);
+    var data = _doNormal(req, res);
+    data.page_name = 'about';
+    res.render('about', data);
+});
+
+router.get('/logout', function(req, res) {
+    req.logout();
+    res.redirect('/login');
 });
 
 module.exports = router;
--- routes/login.js
+++ routes/login.js
@@ -1,14 +1,30 @@
 var express = require('express');
 var router = express.Router();
 var config = require('../lib/config');
+var passport = require('../lib/obm-pass');
+var logger = require('../lib/logger')('oak-bookmark');
 
-/* GET home page. */
 router.get('/', function(req, res) {
+  if(req.isAuthenticated()) {
+      res.redirect('/');
+      return;
+  }
+
   var data = { title: config.site_name,
       login: false,
       page_name: 'login'
   };
+  if(req.session.messages){
+      logger.debug(req.session.messages);
+      data.message = req.session.messages.shift();
+  }
   res.render('login', data);
 });
+
+router.post('/', passport.authenticate('local', {
+      successRedirect: '/', 
+      failureRedirect: '/login',
+      failureMessage: true
+}));
 
 module.exports = router;
