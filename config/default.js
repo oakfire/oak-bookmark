465bbb8d86e66c44f99e08656a26f75eff63c777
oakfire <oakwatcher@gmail.com>
2014-08-20 10:23:19 +0000

    user

--- config/default.js
+++ config/default.js
@@ -1,12 +1,12 @@
 module.exports = {
-    /** 站名 */
     site_name: 'oak bookmark',
-    /** 监听端口 */
     listen_port: 3000,
     /** 日志路径(绝对路径) */
     //log_dir:"",
     /** 日记记录是否不输出到控制台,生产环境请设置为true */
     console_quiet: false,
     /** 日志级别设置, 值可为"silly" "debug" "verbose" "info" "warn" "error" 之一,默认为"debug" */ 
-    log_level: "debug"
+    log_level: 'debug',
+    /** mongodb database name */
+    db_name: 'obm'        
 };
