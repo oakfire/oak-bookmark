a3a709c35cb568248e76a7e21991fcfaabf24708
oakfire <oakwatcher@gmail.com>
2014-12-10 07:14:52 +0000

    YAC

--- routes/login.js
+++ routes/login.js
@@ -5,20 +5,33 @@ var passport = require('../lib/obm-pass');
 var logger = require('../lib/logger')('oak-bookmark');
 
 router.get('/', function(req, res) {
-  if(req.isAuthenticated()) {
-      res.redirect('/');
-      return;
-  }
+    if(req.isAuthenticated()) {
+        res.redirect('/');
+        return;
+    }
 
-  var data = { title: config.site_name,
-      login: false,
-      page_name: 'login'
-  };
-  if(req.session.messages){
-      logger.debug(req.session.messages);
-      data.message = req.session.messages.shift();
-  }
-  res.render('login', data);
+    var data = { 
+        title: config.site_name,
+        login: false,
+        page_name: 'login',
+        last_rememberme: false
+    };
+
+    if(req.session.messages){
+        logger.debug(req.session.messages);
+        logger.debug(req.session);
+        data.message = req.session.messages.shift();
+        data.last_tried_name = req.session.last_tried_name ? req.session.last_tried_name : null;
+        data.last_rememberme = !!req.session.last_rememberme;
+    }
+    res.render('login', data);
+});
+
+router.post('/', function(req, res, next) {
+    logger.info(req.body);
+    req.session.last_tried_name = req.body.username ? req.body.username : null;
+    req.session.last_rememberme = req.body.rememberme ? true : false;
+    next();
 });
 
 router.post('/', passport.authenticate('local', {
