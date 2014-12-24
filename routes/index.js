fda6f0059bab501efd7234341836127876625c2a
oakfire <oakwatcher@gmail.com>
2014-12-24 07:54:28 +0000

    bk view

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var config = require('../lib/config');
+var bookmark = require('../lib/modules/bookmark');
 
 /* GET home page. */
 
@@ -22,7 +23,16 @@ router.get('/', function(req, res) {
 router.get('/index', function(req, res) {
     var data = _doNormal(req, res);
     data.page_name = 'home';
-    res.render('index', data);
+    var bkModel = bookmark(req.user.account);
+    bkModel.find().limit(10).exec(function(err, result){
+        if(err) {
+            res.status(500);
+            res.render('error', {message:'Find bookmark fail', error:err});
+        }else {
+            data.bookmarks = result;
+            res.render('index', data);
+        }
+    })
 });
 
 router.get('/about', function(req, res) {  
